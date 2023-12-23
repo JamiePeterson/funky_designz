047dec4d0a5009990621114d010035157ed3bf23
JP <je.peterson.83@gmail.com>
2023-12-23 22:00:28 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
     const products = [
-        { name: "Product 1", price: "$10.00" },
-        { name: "Product 2", price: "$20.00" },
+        { name: "Product 1", price: "$10.00", description: "Description here" },
+        { name: "Product 2", price: "$20.00", description: "Description here" },
         // Add more products here
     ];
 
     let productList = document.getElementById('product-list');
     products.forEach(product => {
-        let div = document.createElement('div');
-        div.className = 'product';
-        div.innerHTML = `<h3>${product.name}</h3><p>${product.price}</p>`;
-        productList.appendChild(div);
+        let col = document.createElement('div');
+        col.className = 'col-md-4';
+        col.innerHTML = `
+            <div class="card product-card">
+                <div class="card-body">
+                    <h5 class="card-title">${product.name}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <a href="#" class="btn btn-primary">${product.price}</a>
+                </div>
+            </div>
+        `;
+        productList.appendChild(col);
     });
 });
